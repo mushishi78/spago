13bc9347daede39a68b571fb0e0eb54fe7b99c68
Max White <max.white@bluewire-technologies.com>
2017-10-04 19:59:44 +0100

    Extract reverse proxy test into new file

--- server_test.go
+++ server_test.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
 	"path/filepath"
-	"sync"
 	"testing"
 )
 
@@ -192,80 +189,4 @@ func Test_excluded_files_can_be_configured(t *testing.T) {
   </body>
 </html>
 `)
-}
-
-type apiHandler struct{}
-
-func (ah *apiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "API server says hi: "+r.URL.Path)
-}
-
-func Test_uses_reverse_proxy(t *testing.T) {
-	apiServ := &http.Server{Addr: ":3000", Handler: &apiHandler{}}
-
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-
-	go func() {
-		apiServ.ListenAndServe()
-		wg.Done()
-	}()
-
-	go func() {
-		rootDir, close := tTempDir(t)
-		defer close()
-		serv := tServerCreate(t, rootDir)
-		tGetRequestEql(t, serv, "/api/hello", 200, "API server says hi: /api/hello\n")
-		apiServ.Shutdown(nil)
-	}()
-
-	wg.Wait()
-}
-func Test_uses_configured_reverse_proxy_url(t *testing.T) {
-	apiServ := &http.Server{Addr: ":4040", Handler: &apiHandler{}}
-
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-
-	go func() {
-		apiServ.ListenAndServe()
-		wg.Done()
-	}()
-
-	go func() {
-		rootDir, close := tTempDir(t)
-		defer close()
-		tAddConfigFile(t, rootDir, Config{
-			ReverseProxyURL: "http://localhost:4040",
-		})
-		serv := tServerCreate(t, rootDir)
-		tGetRequestEql(t, serv, "/api/hello", 200, "API server says hi: /api/hello\n")
-		apiServ.Shutdown(nil)
-	}()
-
-	wg.Wait()
-}
-func Test_uses_configured_reverse_proxy_route(t *testing.T) {
-	apiServ := &http.Server{Addr: ":3000", Handler: &apiHandler{}}
-
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-
-	go func() {
-		apiServ.ListenAndServe()
-		wg.Done()
-	}()
-
-	go func() {
-		rootDir, close := tTempDir(t)
-		defer close()
-		tAddConfigFile(t, rootDir, Config{
-			ReverseProxyRoute: "/backend",
-		})
-		serv := tServerCreate(t, rootDir)
-		tGetRequestEql(t, serv, "/backend/hello", 200, "API server says hi: /backend/hello\n")
-		apiServ.Shutdown(nil)
-	}()
-
-	wg.Wait()
 }
